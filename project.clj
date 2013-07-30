7af1e3ad1f927db0cc029446ed4ad4f7bd1804c3
Mariano Guerra <mariano@marianoguerra.org>
2013-07-30 15:51:16 +0200

    release tubes 0.2.3 because of issue fixed on pipe dependency

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.marianoguerra/tubes "0.2.3-SNAPSHOT"
+(defproject org.marianoguerra/tubes "0.2.3"
   :description "make ring based web apps by composing small functions"
   :url "http://github.com/marianoguerra/tubes.clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.marianoguerra/pipe "0.1.4-SNAPSHOT"]
+  :dependencies [[org.marianoguerra/pipe "0.1.4"]
                  [org.marianoguerra/rate-meter "0.1.0"]
                  [org.clojure/clojure "1.5.1"]
                  [cheshire "5.0.1"] 
